798ccb83373c72505083d36675be03910f63df06
Sidra Usman <sidra.usman546@gmail.com>
2025-07-10 10:31:35 -0400

    Save everything

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,15 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                NavigationStack {
+                    VStack(spacing: 30) {
+                        Text("Welcome to My App")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+
+                        Text("This is my root view")
+                            .font(.title2)
+                            .foregroundColor(.white.opacity(0.9))
+                            .multilineTextAlignment(.center)
+
+                        NavigationLink(destination: SecondView()) {
+                            Text("Click Me!")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.blue)
+                                .cornerRadius(12)
+                                .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 4)
+                        }
+                        .padding(.horizontal)
+                    }
+                    .padding()
+                    .background(
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color.purple, Color.indigo]),
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
+                    )
+                    .ignoresSafeArea()
+                }
+            }
         }
-        .padding()
-    }
-}
+
+            
+
+        #Preview {
+            ContentView()
+        }
 
 #Preview {
     ContentView()
---
+++ Navigation/SecondView.swift
@@ -0,0 +1,22 @@
+//
+//  SecondView.swift
+//  Navigation
+//
+//  Created by Scholar on 7/10/25.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+           var body: some View {
+               Text("This is the second view!")
+                   .font(.title)
+                   .fontWeight(.semibold)
+                   .padding()
+                   .foregroundColor(.indigo)
+    }
+}
+
+#Preview {
+    SecondView()
+}
